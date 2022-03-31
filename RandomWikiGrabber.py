102faf6128e28adca439fe90beabfcd1bb3f5899
Filip Vyrostko <2479054V@student.gla.ac.uk>
2022-03-31 16:56:42 +0200

    Program to randomly select and display Wikipedia article

---
+++ RandomWikiGrabber.py
@@ -0,0 +1,60 @@
+import requests
+import time
+from random_word import RandomWords
+from bs4 import BeautifulSoup
+
+HOST = "https://wikipedia.org"
+PORT = 80
+
+
+def wait():
+    time.sleep(1)
+
+
+def generate_link(rwg):
+    print("\n\tGenerating article...\n")
+    random_word = rwg.get_random_word()
+
+    r = requests.get(url=HOST + "/wiki/" + random_word)
+
+    # If random word does not have a link, try new
+    while not r.ok:
+        random_word = rwg.get_random_word()
+        r = requests.get(url=HOST + "/wiki/" + random_word)
+
+    return r, random_word
+
+
+def main():
+    print("Welcome to RandomWikiGrabber!"
+          "\nThis tool is used to randomly search and return a Wikipedia article.\n")
+    wait()
+
+    # Generate random word
+    rwg = RandomWords()
+
+    r, random_word = generate_link(rwg)
+
+    while input(f"Are you happy with article about '{random_word}' [y/n]? ") != "y":
+        r, random_word = generate_link(rwg)
+
+    content_div = BeautifulSoup(r.content, features="lxml").body.find('div', attrs={'id': 'mw-content-text'})
+
+    unwanted_tags = ["img", "table", ("div", {"class": "printfooter"}), ("a", {"class": "external text"}),
+                     ("div", {"class": "reflist"})]
+
+    for tag in unwanted_tags:
+
+        if isinstance(tag, tuple):
+            unwanted = content_div.find(tag[0], attrs=tag[1])
+            unwanted.extract()
+
+        else:
+            unwanted = content_div.find(tag)
+            unwanted.extract()
+
+    print(content_div.text.strip())
+
+
+if __name__ == "__main__":
+    main()
